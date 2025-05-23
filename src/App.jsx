eb40759b40da5afc7e441c43da12949ff1b1badb
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-05-23 19:30:03 +0530

    Setup the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold p-5 text-blue-500 bg-yellow-200">
+        {" "}
+        Mahak Counter Project In React
+      </h1>
+    </div>
+  );
+};
+
+export default App;
